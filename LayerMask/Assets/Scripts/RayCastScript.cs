3b9c0f9485b7b36fb7c821b4e4c0d5b8091d0218
MubasshirAhmed <MubasshirAhmed26@Gmail.com>
2018-01-19 23:44:25 +0600

    project added

---
+++ LayerMask/Assets/Scripts/RayCastScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastScript : MonoBehaviour
+{
+    private RaycastHit forwardHit;
+    //public LayerMask layerMaskTest;
+
+    int layer;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out forwardHit, Mathf.Infinity, 1<<8))
+        {
+            Debug.DrawLine(transform.position, forwardHit.point, Color.black);
+        }
+    }
+}
